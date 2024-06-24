a5795e3d53be27a8b7f8bce76b01d30e9dea89a3
Jruz9 <josec300@live.com>
2024-06-24 14:47:13 -0400

    Created spring backend template for the application

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Fitness-Journal
+
+# ABOUT
+----
+Fitness Journal is a application that allows users to track their workouts.
+
+# Features
+---
+- Workout Journal
+- Workout library
+- workout routines lists
+- workout detail home screen. 
+
+# Technologies
+---
+- Backend
+  - Java 17   
+  - Spring boot 3.1.3
+  - mysql
+- Frontend
+  - react
+  - node.js
---
+++ fitness-journal-backend/fitness-journal-backend/src/main/java/com/example/fitness_journal_backend/FitnessJournalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitness_journal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessJournalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessJournalBackendApplication.class, args);
+	}
+
+}

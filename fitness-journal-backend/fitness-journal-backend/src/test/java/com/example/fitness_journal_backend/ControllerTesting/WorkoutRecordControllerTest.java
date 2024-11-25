bcee7755efb841b38953531d68a8c24d11f26f6b
Jose Cruz <34403703+Jruz9@users.noreply.github.com>
2024-11-25 15:36:40 -0500

    Created first controller test for WorkoutRecordControllerTest

--- fitness-journal-backend/fitness-journal-backend/src/test/java/com/example/fitness_journal_backend/ControllerTesting/WorkoutRecordControllerTest.java
+++ fitness-journal-backend/fitness-journal-backend/src/test/java/com/example/fitness_journal_backend/ControllerTesting/WorkoutRecordControllerTest.java
@@ -16,6 +16,15 @@ import com.example.fitness_journal_backend.Entities.WorkoutRecord;
 import com.example.fitness_journal_backend.Services.WorkoutRecordService;
 // used this for the testing implementation of test: https://thepracticaldeveloper.com/guide-spring-boot-controller-tests/#strategy-1-spring-mockmvc-example-in-standalone-mode
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.fitness_journal_backend.ControllerAdvice.WorkoutRecordExceptionHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -31,21 +40,24 @@ public class WorkoutRecordControllerTest {
 
     private  JacksonTester<WorkoutRecord> jsonWorkoutRecord;
 
-
-
+    private MockMvc mvc;
     @BeforeEach
     public void setup(){
         JacksonTester.initFields(this, new ObjectMapper());
         //TODO: Create the controller advice class and filter class testing.
 
-        // mvc= MockMvcBuilders.standaloneSetup(workoutRecordController)
-        // .setControllerAdvice(new ResponseStatusException(HttpStatus.NOT_FOUND,"Workout Record is not found."))
-        // .addFilters("null")
-        // .build();
+         mvc= MockMvcBuilders.standaloneSetup(workoutRecordController)
+         .setControllerAdvice(new WorkoutRecordExceptionHandler())
+         .addFilters()
+         .build();
     }
 
     @Test
-    public void createWOrkoutRecordTest(WorkoutRecord w) throws Exception{
-        
+    public void createWorkoutRecordTest(WorkoutRecord w) throws Exception{
+        MockHttpServletResponse response= mvc.perform(
+                post("/").contentType(MediaType.APPLICATION_JSON).content(jsonWorkoutRecord.write(new WorkoutRecord()).getJson())
+        ).andReturn().getResponse();
+        //then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
     }
 }

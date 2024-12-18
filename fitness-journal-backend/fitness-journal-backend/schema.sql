689ba8342e120ee4841e699d4aba46375e6c31ba
Jose Cruz <34403703+Jruz9@users.noreply.github.com>
2024-12-18 10:32:38 -0500

    Changed order of table formation to make the tables work with mysql.

--- fitness-journal-backend/fitness-journal-backend/schema.sql
+++ fitness-journal-backend/fitness-journal-backend/schema.sql
@@ -1,17 +1,19 @@
-create table if not exists workout(
-    id  bigint auto_increment,
-    session int default 0,
-    reps int default 0,
-    weight float default 0.0,
-    workoutrecordid bigint not null,
-    primary key(id),
-    foreign key (workoutrecordId) references workoutrecord(workoutrecordid)
-);
-
+-- CREATE SCHEMA `fitness_journal` ;
+-- Order Matter for which tables first.:
 create table if not exists workoutrecord(
     workoutrecordid bigInt auto_increment,
-    workoutname var(255) default '',
-    workoutdate datetime default getDate(),
-    primary key(workoutRecordId),
-    -- decided not add foreign key to test if its needed to make the connection
+    workoutname varchar(255) default '',
+    workoutdate datetime default Now(),
+    primary key(workoutRecordId)
+);
+
+CREATE TABLE IF NOT EXISTS workout (
+    id BIGINT AUTO_INCREMENT,
+    sessions INT DEFAULT 0,
+    reps INT DEFAULT 0,
+    weight FLOAT DEFAULT 0.0,
+    workoutrecordid BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (workoutrecordId)
+        REFERENCES workoutrecord (workoutrecordid)
 );
